130e6c68aa816de30931c2191b95be05d7f8a22a
Timalice1 <newacccc2@gmail.com>
2025-09-11 15:28:27 +0300

    Add save game object and actor save data record struct

---
+++ Source/RTS/Public/SaveGame/RTS_SaveGame.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "RTS_SaveGame.generated.h"
+
+/// Generic struct that represents actor saved data record
+USTRUCT()
+struct FActorSaveDataRecord
+{
+	GENERATED_BODY()
+
+	UPROPERTY(SaveGame)
+	FString ActorName;
+
+	UPROPERTY(SaveGame)
+	UClass* ActorClass;
+
+	UPROPERTY(SaveGame)
+	FTransform ActorTransform;
+
+	UPROPERTY(SaveGame)
+	TArray<uint8> ByteData;
+};
+
+/// Default save game object, used for saving game-specific data
+UCLASS(Abstract)
+class RTS_API URTS_SaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TArray<FActorSaveDataRecord> ActorSaveRecords;
+};
